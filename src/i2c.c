4a9299f0909617fc226fe9c961805e82a264f100
KeitetsuWorks <keitetsu.jp@outlook.com>
2017-07-04 23:11:02 +0900

    include文を整理

--- src/i2c.c
+++ src/i2c.c
@@ -9,10 +9,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
